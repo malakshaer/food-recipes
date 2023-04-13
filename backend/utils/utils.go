ffbc6fe05b707ea4522b4e25ab209c19791d34a6
malakshaer <shaermk06@gmail.com>
2023-04-13 08:48:52 +0300

    backend: add function to get id from session

--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -4,14 +4,24 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/base64"
+	"errors"
 	"golang-food-recipes/database"
 	"golang-food-recipes/models"
+	"net/http"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	// "github.com/gin-contrib/sessions"
+	"github.com/gorilla/sessions"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
+var store sessions.Store
+
+func init() {
+	store = sessions.NewCookieStore([]byte("secret-key"))
+}
 
 // Get user by email
 func GetUserByEmail(email string) (models.User, error) {
@@ -57,4 +67,32 @@ func GenerateToken(user models.User) (string, error) {
 	}
 
 	return signedToken, nil
+}
+
+// Check if password is correct
+func CheckPassword(password string) (models.User, error) {
+	var user models.User
+	collection := database.OpenCollection("users")
+	err := collection.FindOne(context.Background(), bson.M{"password": password}).Decode(&user)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
+}
+
+// Check if user is logged in
+func GetUserIDFromSession(r *http.Request, store *sessions.CookieStore) (string, error) {
+	// Get the session from the cookie store.
+	session, err := store.Get(r, "session-name")
+	if err != nil {
+		return "", err
+	}
+
+	// Get the user ID from the session.
+	userID, ok := session.Values["userID"].(string)
+	if !ok || userID == "" {
+		return "", errors.New("invalid userID in session")
+	}
+
+	return userID, nil
 }
