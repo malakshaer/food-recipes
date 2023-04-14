4b54a220a7a41b25149863aadc10ce3bce227f7e
malakshaer <shaermk06@gmail.com>
2023-04-14 17:27:53 +0300

    backend: add recipe routes

--- backend/routes/recipeRoutes.go
+++ backend/routes/recipeRoutes.go
@@ -8,5 +8,8 @@ import (
 
 func RecipeRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("/recipe", controller.CreateRecipe())
-
+	incomingRoutes.GET("/recipe", controller.GetAllRecipes())
+	incomingRoutes.GET("/recipe/:id", controller.GetRecipeById())
+	incomingRoutes.PUT("/recipe/:id", controller.UpdateRecipeById())
+	incomingRoutes.DELETE("/recipe/:id", controller.DeleteRecipeById())
 }
