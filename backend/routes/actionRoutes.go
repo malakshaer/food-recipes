28d86229ebdffb9e284c38e1a444800d528950f9
malakshaer <shaermk06@gmail.com>
2023-04-15 15:24:54 +0300

    backend: add unlikeRecipe function to the route

--- backend/routes/actionRoutes.go
+++ backend/routes/actionRoutes.go
@@ -10,4 +10,5 @@ func ActionRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("save_recipe/:id", controller.SaveRecipe())
 	incomingRoutes.POST("unsave_recipe/:id", controller.UnSaveRecipe())
 	incomingRoutes.POST("like_recipe/:id", controller.LikeRecipe())
+	incomingRoutes.POST("unlike_recipe/:id", controller.UnLikeRecipe())
 }
