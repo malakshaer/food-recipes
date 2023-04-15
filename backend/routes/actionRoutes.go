bd74cb872db3640193afef7d9b1e0d3783950a37
malakshaer <shaermk06@gmail.com>
2023-04-15 15:12:59 +0300

    backend:  add like recipe function to the route

--- backend/routes/actionRoutes.go
+++ backend/routes/actionRoutes.go
@@ -9,4 +9,5 @@ import (
 func ActionRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("save_recipe/:id", controller.SaveRecipe())
 	incomingRoutes.POST("unsave_recipe/:id", controller.UnSaveRecipe())
+	incomingRoutes.POST("like_recipe/:id", controller.LikeRecipe())
 }
