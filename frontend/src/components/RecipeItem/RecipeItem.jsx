59444af2c99eedddd8af0053cf747c42f833f699
malakshaer <shaermk06@gmail.com>
2023-04-16 14:53:48 +0300

    frontend: modify card style and content

--- frontend/src/components/RecipeItem/RecipeItem.jsx
+++ frontend/src/components/RecipeItem/RecipeItem.jsx
@@ -1,26 +1,36 @@
+import { useRouter } from "next/router";
 import classes from "./RecipeItem.module.css";
+import Image from "next/image";
+import recipeImage from "../../../public/Spaghetti.jpg";
 
 const RecipeItem = (props) => {
+  const router = useRouter();
+
+  const showRecipeDetails = () => {
+    router.push("/" + props.id);
+  };
+
   return (
-    <li className={classes.item}>
-      <div className={classes.card}>
-        <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
-        </div>
-        <div className={classes.content}>
-          <h3>{props.name}</h3>
-          <ul>
-            {props.ingredients.map((ingredient, index) => (
-              <li key={index}>{ingredient.text}</li>
-            ))}
-          </ul>
-          <p>{props.instructions}</p>
-        </div>
-        <div>
-          <button className={classes.actions}>Show</button>
-        </div>
+    <div className={classes.card}>
+      <div className={classes.image}>
+        <Image src={recipeImage} alt="recipe-image" priority="high" />
       </div>
-    </li>
+      <div className={classes.content}>
+        <h1>{props.name}</h1>
+        <p>({props.category})</p>
+        <div className={classes.time}>
+          <h3>⏱</h3>
+          <p>{props.total_time} minutes</p>
+        </div>
+        <h3>Created At:</h3>
+        <p>{props.created_at}</p>
+      </div>
+      <div>
+        <button className={classes.actions} onClick={showRecipeDetails}>
+          Show
+        </button>
+      </div>
+    </div>
   );
 };
 
