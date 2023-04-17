6682f1a05083afb938d9752f18ae266ebe06fc81
malakshaer <shaermk06@gmail.com>
2023-04-17 14:18:56 +0300

    frontend: add header and update categories

--- frontend/src/components/CreateRecipe/CreateRecipe.jsx
+++ frontend/src/components/CreateRecipe/CreateRecipe.jsx
@@ -35,59 +35,69 @@ const CreateRecipe = (props) => {
   return (
     <div className={classes.card}>
       <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="name">Recipe Name:</label>
-          <input type="text" required id="name" ref={nameInputRef} />
+        <h2>Unleash your creativity and let your taste buds dance with joy</h2>
+        <div>
+          <div className={classes.control}>
+            <label htmlFor="name">Recipe Name:</label>
+            <input type="text" required id="name" ref={nameInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="image">Recipe Image:</label>
+            <input type="file" required id="image" ref={imageInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="category">Category:</label>
+            <select
+              className={classes.category}
+              id="category"
+              required
+              ref={categoryInputRef}
+            >
+              <option value="">Select Category</option>
+              <option value="salads">Salads</option>
+              <option value="soups">Soups</option>
+              <option value="desserts">Desserts</option>
+              <option value="main-dishes">Main Dishes</option>
+              <option value="sides">Sides</option>
+              <option value="breakfast">Breakfast</option>
+              <option value="drinks">Drinks</option>
+              <option value="snacks">Snacks</option>
+            </select>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="totalTime">Total Time:</label>
+            <input
+              type="number"
+              required
+              id="totalTime"
+              ref={totalTimeInputRef}
+              min="0"
+            />
+          </div>
         </div>
-        <div className={classes.control}>
-          <label htmlFor="image">Recipe Image:</label>
-          <input type="file" required id="image" ref={imageInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="category">Category:</label>
-          <select
-            className={classes.category}
-            id="category"
-            required
-            ref={categoryInputRef}
-          >
-            <option value="">Select Category</option>
-            <option value="meal">Meal</option>
-            <option value="breakfast">Breakfast</option>
-            <option value="dessert">Dessert</option>
-          </select>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="totalTime">Total Time:</label>
-          <input
-            type="number"
-            required
-            id="totalTime"
-            ref={totalTimeInputRef}
-            min="0"
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="ingredients">Ingredients:</label>
-          <textarea
-            id="ingredients"
-            required
-            rows="5"
-            ref={ingredientsInputRef}
-            placeholder="Insert (;) after each ingredients"
-          ></textarea>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="instructions">Instructions:</label>
-          <textarea
-            id="instructions"
-            required
-            rows="5"
-            ref={instructionsInputRef}
-          ></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button>Add Recipe</button>
+        <div>
+          <div className={classes.control}>
+            <label htmlFor="ingredients">Ingredients:</label>
+            <textarea
+              id="ingredients"
+              required
+              rows="5"
+              ref={ingredientsInputRef}
+              placeholder="Insert (;) after each ingredients"
+            ></textarea>
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="instructions">Instructions:</label>
+            <textarea
+              id="instructions"
+              required
+              rows="5"
+              ref={instructionsInputRef}
+            ></textarea>
+          </div>
+          <div className={classes.actions}>
+            <button>Add Recipe</button>
+          </div>
         </div>
       </form>
     </div>
