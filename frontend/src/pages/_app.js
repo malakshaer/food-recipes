04bfcabddda492dceca2f8219dcc68e85077b571
malakshaer <shaermk06@gmail.com>
2023-04-17 17:49:20 +0300

    frontend: remove the layout from login and register

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,11 +1,20 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout/Layout";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const excludeLayoutPaths = ["/login", "/register"];
+
+  const shouldExcludeLayout = excludeLayoutPaths.some(
+    (path) => path === router.pathname
+  );
+
   return (
-    <Layout>
+    <>
+      {!shouldExcludeLayout && <Layout />}
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
 
