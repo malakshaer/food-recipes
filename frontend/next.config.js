44ffa8adf18ace33552c3bb6c3ee7b03a11f3b2b
malakshaer <shaermk06@gmail.com>
2023-04-18 14:21:09 +0300

    frontend: add api endPoint to next configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const { parsed: localEnv } = require("dotenv").config();
+const webpack = require("webpack");
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+    return config;
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_ENDPOINT}/api/:path*`,
+      },
+    ];
+  },
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+};
