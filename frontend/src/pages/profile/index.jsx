31d2d3c658af11031593ca6ae80a69edd6002a04
malakshaer <shaermk06@gmail.com>
2023-04-19 15:33:48 +0300

    frontend: get user info from backend

--- frontend/src/pages/profile/index.jsx
+++ frontend/src/pages/profile/index.jsx
@@ -1,13 +1,15 @@
-import { useRouter } from "next/router";
 import Image from "next/image";
 import { FaEdit } from "react-icons/fa";
 import { RiAddBoxLine } from "react-icons/ri";
-import classes from "./Profile.module.css";
-import ProfileImage from "../../../public/Spaghetti.jpg";
+import { useRouter } from "next/router";
+import axios from "axios";
 import ProfileNavBar from "../../components/ProfileNavBar/ProfileNavBar";
+import classes from "./Profile.module.css";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
   const router = useRouter();
+  const [user, setUser] = useState();
 
   const handleEditAccount = () => {
     router.push("/edit-profile");
@@ -17,17 +19,47 @@ const Profile = () => {
     router.push("/create-recipe");
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const authHeaders = {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        };
+        const response = await axios.get(`${process.env.API_ENDPOINT}user`, {
+          headers: authHeaders,
+        });
+        const user = response.data.data;
+        setUser(user);
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
   return (
     <div>
       <div className={classes.profile}>
         <div className={classes.profileLeft}>
           <div className={classes.profileImage}>
-            <Image src={ProfileImage} width={150} height={150} alt="Profile" />
+            <img
+              src={`data:image/*;base64,${user.profileimage}`}
+              alt="profile"
+            />
           </div>
         </div>
         <div className={classes.profileCenter}>
-          <h1 className={classes.profileName}>Malak Shaer</h1>
-          <p className={classes.profileBio}>Full Stack Developer</p>
+          {user ? (
+            <>
+              <h1 className={classes.profileName}>{user.username}</h1>
+              <p className={classes.profileBio}>{user.profilebio}</p>
+            </>
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
         <div className={classes.profileRight}>
           <button className={classes.profileButton} onClick={handleEditAccount}>
@@ -46,3 +78,25 @@ const Profile = () => {
 };
 
 export default Profile;
+
+export async function getServerSideProps(context) {
+  try {
+    const token = context.req.headers.authorization?.replace("Bearer ", "");
+    const authHeaders = {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+    const response = await axios.get(`${process.env.API_ENDPOINT}user`, {
+      headers: authHeaders,
+    });
+    const user = response.data;
+    return {
+      props: { user },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: { user: null },
+    };
+  }
+}
