d752a21c10dd1b95121482176744679c7c1108ff
malakshaer <shaermk06@gmail.com>
2023-05-05 13:11:07 +0300

    backend: modify saved and liked recipes

--- backend/controllers/actionController.go
+++ backend/controllers/actionController.go
@@ -39,18 +39,7 @@ func SaveRecipe() gin.HandlerFunc {
 		}
 		// Create saved recipe
 		var saved models.SavedRecipes
-		saved.ID = primitive.NewObjectID()
 		saved.RecipeID = id
-		saved.RecipeAuthorID = user.(models.User).ID
-		saved.Name = recipe.Name
-		saved.Ingredients = recipe.Ingredients
-		saved.Instructions = recipe.Instructions
-		saved.TotalTime = recipe.TotalTime
-		saved.Category = recipe.RecipeCategory
-		saved.RecipeImage = recipe.RecipeImage
-		saved.RecipeAuthorName = user.(models.User).Username
-		saved.RecipeAuthorImage = user.(models.User).ProfileImage
-		saved.RecipeAuthorBio = user.(models.User).ProfileBio
 
 		// Update the user's saved recipes
 		if _, err := userCollection.UpdateOne(context.Background(), bson.M{"_id": user.(models.User).ID}, bson.M{"$push": bson.M{"savedrecipes": saved}}); err != nil {
@@ -138,7 +127,6 @@ func LikeRecipe() gin.HandlerFunc {
 
 		// Create liked recipe
 		var liked models.LikedRecipes
-		liked.ID = primitive.NewObjectID()
 		liked.RecipeID = id
 
 		// Update the user's liked recipes
